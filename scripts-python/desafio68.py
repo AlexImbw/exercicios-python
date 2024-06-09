910af2b3ac0e890eae1fe9275927f43bb97bd21a
Alex Santana <aleximbw@gmail.com>
2024-06-09 13:53:49 -0300

    Desafio 68 pronto

--- scripts-python/desafio68.py
+++ scripts-python/desafio68.py
@@ -7,6 +7,8 @@ par = 0
 impar = 0
 soma = 0
 vitorias = 0
+derrota = 0
+
 
 computador = random.randint(1, 10)
 jogador = int(input('Digite um número: '))
@@ -14,28 +16,70 @@ op = str(input('Você quer [P/I] ?: ')).upper().strip()
 
 soma = computador + jogador
 
-while True:
+if op == 'P' and soma % 2 == 0:
+        print('-'*20)
+        print('Você Venceu!')
+        print('-'*20)
+        vitorias += 1
+        print(f'Numero do computador: {computador}')
+
+elif op == 'I' and soma % 2 > 0:
+        print('-'*20)
+        print('Você Venceu!')
+        print('-'*20)
+        vitorias += 1
+        print(f'Numero do computador: {computador}')
+
+        soma = computador + jogador
+elif op == 'P' and soma % 2 > 0:
+        print('-'*20)
+        print('Você perdeu!')
+        print('-'*20)
+        derrota = 1
+        
+elif op == 'I' and soma % 2 == 0:
+        print('-'*20)
+        print('Você Perdeu!')
+        print('-'*20)
+        derrota = 1
+
+while derrota != 1:
+
+    computador = random.randint(1, 10)
+    jogador = int(input('Vamos jogar novamente!: '))
+    op = str(input('Você quer [P/I] ?: ')).upper().strip()
+
+    soma = computador + jogador
 
     if op == 'P' and soma % 2 == 0:
+        print('-'*20)
         print('Você Venceu!')
+        print('-'*20)
         vitorias += 1
+        print(f'Numero do computador: {computador}')
+
     elif op == 'I' and soma % 2 > 0:
+        print('-'*20)
         print('Você Venceu!')
+        print('-'*20)
         vitorias += 1
-        
-        computador = random.randint(1, 10)
-        jogador = int(input('Digite um número: '))
-        op = str(input('Você quer [P/I] ?: ')).upper().strip()
+        print(f'Numero do computador: {computador}')
 
         soma = computador + jogador
     elif op == 'P' and soma % 2 > 0:
+        print('-'*20)
         print('Você perdeu!')
+        print('-'*20)
+        derrota = 1
         
     elif op == 'I' and soma % 2 == 0:
+        print('-'*20)
         print('Você Perdeu!')
-
+        print('-'*20)
+        derrota = 1
 print('-'*20)
 print(f'Total de vitórias: {vitorias}.')
 print('-'*20)
+print(f'Numero do computador: {computador}.')
 
 
