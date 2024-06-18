237d80712f2d4fd4b3120100c892b741356112c6
Alex Santana <aleximbw@gmail.com>
2024-06-17 21:28:30 -0300

    Tentativas diferentes 74 e 75

--- scripts-python/desafio74.py
+++ scripts-python/desafio74.py
@@ -2,16 +2,15 @@ from random import randint
 
 maior = 0
 menor = 0
+n1 = 0
 
-n1 = randint(0,9)
-n2 = randint(0,9)
-n3 = randint(0,9)
-n4 = randint(0,9)
-n5 = randint(0,9)
+for n1 in range(0,5):
+    n1 = (randint(0,9), randint(0,9), randint(0,9), randint(0,9), randint(0,9))
 
-sorteio = n1, n2, n3, n4, n5
+
 print('='*45)
-print(f'Os valores sorteados foram: {sorteio}')
+print(f'Os valores sorteados foram: {n1}')
 print('='*45)
+
 print(f'O maior valor foi {maior}')
 print(f'O menor valor foi {menor}')
---
+++ scripts-python/desafio75.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+n3 = int(input('Digite um valor: '))
+n4 = int(input('Digite um valor: '))
+
+valor = n1, n2, n3, n4
+
